fad26cbaaf3475bae29e142030ffd4c4fbf56bdc
Szymonito <Szymon.Nowakowski@cyfroweszkoly.pl>
2024-12-15 20:04:58 +0100

    app js start

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import * as  AnimalController from "./controllers/AnimalController.js";
+
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/animals", AnimalController.getAllAnimals)
+app.get("/animals/select/:id", AnimalController.getAnimalById)
+app.get("/animals/endangered", AnimalController.getEndangeredAnimals)
+app.get("/animals/habitat/:habitat", AnimalController.getAnimalsByHabitat)
+app.get("animals/species/:species", AnimalController.getAnimalsBySpecies)
+app.post("/animals", AnimalController.addAnimal)
+app.put("/animals/:id", AnimalController.updateAnimal)
+app.delete("/animals/:id", AnimalController.deleteAnimal)
+
+app.listen (port, () => {
+    console.log("Server started on port " + port)
+})
